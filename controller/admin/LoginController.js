345a97720aa131e33761720ca87ac281a503402e
hndryno <hendriyono97@gmail.com>
2023-02-04 11:55:40 +0700

    add user admin seeder, add login for admin

---
+++ controller/admin/LoginController.js
@@ -0,0 +1,53 @@
+const UserAdmin = require('../../models').UserAdmins
+const bcrypt = require('bcryptjs')
+
+const jwt = require("jsonwebtoken")
+require('dotenv').config()
+
+class Login{
+    async exec (req, res, next){
+        try{
+
+            let email = req.body.email;
+            let password = req.body.password;
+
+            let findEmail = await UserAdmin.findOne({email})
+
+            if(!findEmail)throw new Error("Username atau password salah");
+
+            let findPassword = findEmail.password;
+
+            let ismatch = await bcrypt.compare(
+                password,
+                findPassword
+            )
+
+            if(!ismatch){
+                throw new Error('maaf password tidak sesuai')
+            }
+
+            let payload = {
+                id: findEmail.id,
+                name: findEmail.fullName,
+                email: findEmail.email,
+            }
+
+            let token = await jwt.sign(payload, process.env.JWT_SECRET,{
+                expiresIn: '1m'
+            })
+            
+            return res.status(200).json({
+                status: 'success',
+                message: 'data berhasil ditampilkan',
+                data: {payload, token}
+            })
+        }catch(err){
+            return res.status(400).json({
+                status: 'Error!',
+                message: err.message
+            })
+        }
+    }
+}
+
+module.exports = Login
--- controller/user/RegisterController.js
+++ controller/user/RegisterController.js
@@ -18,42 +18,6 @@ class RegisterController{
 
             let data = await User.create(userData)
             
-            // if(req.user.is_active == false){
-            //     throw Error('user status is not active')
-            // }
-
-            // let last_ip_login = req.connection.remoteAddress
-            // let last_login = new Date()
-
-            // let payload = {
-            //     user_id: req.user._id,
-            //     user_name: req.user.name,
-            //     user_email: req.user.email,
-            // }
-
-            // let privateKey = fs.readFileSync('./public/private.key')
-
-            // let i  = 'Nusatrades corp';          // Issuer 
-            // let s  = 'admin@admin.com';        // Subject 
-            // let a  = 'http://nusatrades.in'; // Audience// SIGNING OPTIONS
-
-            // let signOptions = {
-            // issuer:  i,
-            // subject:  s,
-            // audience:  a,
-            // expiresIn:  "12h",
-            // algorithm:  "RS256"
-            // };
-
-            // let token = await jwt.sign(payload, privateKey, signOptions)
-
-            // await SuperUser.update({last_ip_login: last_ip_login, last_login: last_login },{where : {email: req.user.email}})
-
-            // return res.status(200).json({
-            //     status: 'success',
-            //     data: {token, expiresIn: '12 hours'},
-            //     message: 'login successfull'
-            // })
              return res.status(200).json({
                 status: 'success',
                 data: {data, expiresIn: '12 hours'},
