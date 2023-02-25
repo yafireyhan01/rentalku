d79e8a4dd2cc0747e374294436215eb16a8f073b
yafireyhan01 <yafireyhan01@gmail.com>
2023-02-25 10:29:58 +0700

    Update dependecies

--- seeders/20230204043010-admin-user.js
+++ seeders/20230204043010-admin-user.js
@@ -1,22 +1,23 @@
-'use strict';
-const bcrypt = require('bcryptjs')
-
+"use strict";
+const bcrypt = require("bcryptjs");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    let password = await bcrypt.hash('test1234', 8)
+  async up(queryInterface, Sequelize) {
+    let password = await bcrypt.hash("123qwe", 8);
 
-    return queryInterface.bulkInsert('user_admins', [{
-      full_name: 'rafi',
-      email: 'rafiaceng@gmail.com',
-      password: password,
-      created_at: new Date(),
-      updated_at: new Date()
-    }]);
+    return queryInterface.bulkInsert("user_admins", [
+      {
+        full_name: "admin",
+        email: "admin@gmail.com",
+        password: password,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    return queryInterface.bulkDelete('user_admins', null, {});
-  }
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("user_admins", null, {});
+  },
 };
